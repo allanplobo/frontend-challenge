d50b54993fab4819a7e5b7aed8b2a62e6235b462
Allan Lobo <allanplobo@gmail.com>
2020-10-22 12:27:06 -0300

    Add modal + css adjusts

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,106 +1,146 @@
 const urlItems = "https://br.ongame.net/api/challenge/items/";
-var rewardsDiv = document.getElementById('rewardsDiv');
+
+function openConfirmModal(id, image, name) {
+    let darkBackground = document.getElementById('darkBackground');
+    darkBackground.style.display = 'block';
+
+    let modalDiv = document.getElementById('modal');
+    modalDiv.style.display = 'flex';
+
+    let modalImageItem = document.createElement('img');
+    modalImageItem.src = image;
+    modalDiv.appendChild(modalImageItem);
+
+    let modalNameItem = document.createElement('p');
+    modalNameItem.innerHTML = name;
+    modalDiv.appendChild(modalNameItem);
+
+    let modalButtonsDiv = document.createElement('div');
+    modalButtonsDiv.classList.add('modal-buttons-div');
+    modalDiv.appendChild(modalButtonsDiv);
+
+    let modalConfirmButton = document.createElement('button');
+    modalConfirmButton.id = 'confirmButton';
+    modalConfirmButton.classList.add('confirm-button');
+    modalConfirmButton.innerHTML = 'SIM';
+    modalButtonsDiv.appendChild(modalConfirmButton);
+
+    let modalDenyButton = document.createElement('button');
+    modalDenyButton.id = 'denyButton';
+    modalDenyButton.classList.add('deny-button');
+    modalDenyButton.innerHTML = 'NÃO';
+    modalButtonsDiv.appendChild(modalDenyButton);
+
+    console.log(id, image, name);
+}
 
 var xhttp = new XMLHttpRequest();
+
 xhttp.open("GET", urlItems, true);
-xhttp.onload = function(e) {
-    if (xhttp.readyState === 4) {
-        if (xhttp.status === 200) {
-            console.log(xhttp.responseText);
-            var items = JSON.parse(xhttp.responseText);
 
-            for (var item in items) {
-                let percentage = items[item].percentage;
-                let newItems = document.createElement('div');
-                newItems.classList.add('item');
-                newItems.id = items[item].id;
+xhttp.onload = (e) => {
 
-                // DIV IMAGE + Childs
-                let newItemImageDiv = document.createElement('div');
-                newItemImageDiv.classList.add('image');
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
 
-                let newItemsImage = document.createElement('img');
-                newItemsImage.src = items[item].image;
-                newItemsImage.style.width = "100%";
-                newItemsImage.alt = 'item picture';
-                // INSERT IMAGE IN IMAGE DIV
-                newItemImageDiv.appendChild(newItemsImage);
-
-                // DIV DESCRIPTION + Childs
-                let newDescriptionDiv = document.createElement('div');
-                newDescriptionDiv.classList.add('description');
-
-                // SUPERIOR DIV OF DESCRIPTION
-                let newNameButtonDiv = document.createElement('div');
-                newNameButtonDiv.classList.add('name-button');
-                newDescriptionDiv.appendChild(newNameButtonDiv)
-
-                let newItemName = document.createElement('p');
-                newItemName.classList.add('item-name');
-                newItemName.innerHTML = items[item].name;
-                newNameButtonDiv.appendChild(newItemName);
-
-                // BUTTON + PROGRESS BAR
-                if (items[item].percentage < 100 && !items[item].redeemed) {
-                    let newReedemButton = document.createElement('button');
-                    let newProgressBar = document.createElement('span');
-
-                    newReedemButton.innerHTML = 'RESGATAR';
-                    newNameButtonDiv.appendChild(newReedemButton);
-
-                    newProgressBar.classList.add('progress-bar');
-                    newProgressBar.style.width = percentage + "%";
-                    newDescriptionDiv.appendChild(newProgressBar);
-
-                } else if (items[item].percentage == 100 && !items[item].redeemed) {
-                    let newCanReedemButton = document.createElement('button');
-                    let newProgressBar = document.createElement('span');
+        var items = JSON.parse(xhttp.responseText);
+        let rewardsDiv = document.getElementById('rewardsDiv');
 
 
-                    newCanReedemButton.innerHTML = 'RESGATAR';
-                    newCanReedemButton.classList.add('can-reddem-button');
-                    newNameButtonDiv.appendChild(newCanReedemButton);
+        for (var item in items) {
+            let percentage = items[item].percentage;
+            let newItems = document.createElement('div');
+            newItems.classList.add('item');
+            newItems.id = items[item].id;
 
-                    newProgressBar.classList.add('progress-bar');
-                    newProgressBar.style.width = percentage + "%";
-                    newProgressBar.classList.add('can-reddem-progress-bar');
-                    newDescriptionDiv.appendChild(newProgressBar);
+            // DIV IMAGE + Childs
+            let newItemImageDiv = document.createElement('div');
+            newItemImageDiv.classList.add('image');
+
+            let newItemsImage = document.createElement('img');
+            newItemsImage.src = items[item].image;
+            newItemsImage.style.width = "100%";
+            newItemsImage.alt = 'item picture';
+            // INSERT IMAGE IN IMAGE DIV
+            newItemImageDiv.appendChild(newItemsImage);
+
+            // DIV DESCRIPTION + Childs
+            let newDescriptionDiv = document.createElement('div');
+            newDescriptionDiv.classList.add('description');
+
+            // SUPERIOR DIV OF DESCRIPTION
+            let newNameButtonDiv = document.createElement('div');
+            newNameButtonDiv.classList.add('name-button');
+            newDescriptionDiv.appendChild(newNameButtonDiv)
+
+            let newItemName = document.createElement('p');
+            newItemName.classList.add('item-name');
+            newItemName.innerHTML = items[item].name;
+            newNameButtonDiv.appendChild(newItemName);
+
+            // BUTTON + PROGRESS BAR
+            if (items[item].percentage < 100 && !items[item].redeemed) {
+                let newReedemButton = document.createElement('button');
+                let newProgressBar = document.createElement('span');
+
+                newReedemButton.innerHTML = 'RESGATAR';
+                newNameButtonDiv.appendChild(newReedemButton);
+
+                newProgressBar.classList.add('progress-bar');
+                newProgressBar.style.width = percentage + "%";
+                newDescriptionDiv.appendChild(newProgressBar);
+
+            } else if (items[item].percentage == 100 && !items[item].redeemed) {
+                let newCanReedemButton = document.createElement('button');
+                let newProgressBar = document.createElement('span');
 
 
-                } else {
-                    let doneDiv = document.createElement('div');
-                    doneDiv.style = 'display: flex; align-items: center;'
-                    let newReedemedButton = document.createElement('p');
-                    let newDoneIcon = document.createElement('img');
-                    newDoneIcon.style.width = '24px';
+                newCanReedemButton.innerHTML = 'RESGATAR';
+                newCanReedemButton.classList.add('can-reddem-button');
+                newCanReedemButton.onclick = 'openConfirmModal()';
+                newCanReedemButton.addEventListener("click", () => {
+                    openConfirmModal(newItems.id, newItemsImage.src, newItemName.innerHTML)
+                }, false);
+                newNameButtonDiv.appendChild(newCanReedemButton);
 
-                    newDoneIcon.src = './assets/img/done.svg';
-                    newDoneIcon.alt = 'done icon';
-                    newReedemedButton.innerHTML = "ITEM RESGATADO";
+                newProgressBar.classList.add('progress-bar');
+                newProgressBar.style.width = percentage + "%";
+                newProgressBar.classList.add('can-reddem-progress-bar');
+                newDescriptionDiv.appendChild(newProgressBar);
 
-                    newNameButtonDiv.appendChild(doneDiv);
-                    doneDiv.appendChild(newDoneIcon);
-                    doneDiv.appendChild(newReedemedButton);
-                }
 
-                // PROGRESS BAR 
-                if (!items[item].redeemed) {
+            } else {
+                let doneDiv = document.createElement('div');
+                doneDiv.style = 'display: flex; align-items: center;'
+                let newReedemedButton = document.createElement('p');
+                let newDoneIcon = document.createElement('img');
+                newDoneIcon.style.width = '24px';
 
-                }
-                if (items[item].percentage == 100 && !items[item].redeemed) {
+                newDoneIcon.src = './assets/img/done.svg';
+                newDoneIcon.alt = 'done icon';
+                newReedemedButton.innerHTML = "ITEM RESGATADO";
 
-                }
+                newNameButtonDiv.appendChild(doneDiv);
+                doneDiv.appendChild(newDoneIcon);
+                doneDiv.appendChild(newReedemedButton);
+            }
 
-                rewardsDiv.appendChild(newItems);
-                newItems.appendChild(newItemImageDiv);
-                newItems.appendChild(newDescriptionDiv);
+            // PROGRESS BAR 
+            if (!items[item].redeemed) {
+
+            }
+            if (items[item].percentage == 100 && !items[item].redeemed) {
 
             }
 
-            console.log(items);
-        } else {
-            console.log('erroy');
+            rewardsDiv.appendChild(newItems);
+            newItems.appendChild(newItemImageDiv);
+            newItems.appendChild(newDescriptionDiv);
+
         }
+
+        // console.log(items);
+    } else {
+        console.log('erroy');
     }
 }
 xhttp.send();
